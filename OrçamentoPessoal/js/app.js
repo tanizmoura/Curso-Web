b69d875e7564b22979bd1282ebcca7f2740fa032
Tanise Moura <tanise.mourax3@hotmail.com>
2020-07-04 00:15:04 -0300

    Update app.js

--- OrçamentoPessoal/js/app.js
+++ OrçamentoPessoal/js/app.js
@@ -1,1 +1,61 @@
-alert('ok')
+class Despesa {
+	constructor(ano, mes, dia, tipo, descricao, valor) {
+		this.ano = ano
+		this.mes = mes
+		this. dia = dia
+		this.tipo = tipo
+		this.descricao = descricao
+		this.valor = valor
+	}
+
+	validarDados() {
+		for (let i in this) {
+			if (this[i] === null || this[i] === undefined || this[i] === '') {
+				return false
+			}
+		}
+		return true
+	}
+}
+
+class Bd {
+	constructor() {
+		let id = localStorage.getItem('id')
+
+		if (id === null) {
+			localStorage.setItem('id', 0)
+		}
+	}
+
+	getProximoId() {
+		let id = localStorage.getItem('id')
+		return parseInt(id) + 1
+	}
+
+	gravar(d) {
+		let id = this.getProximoId()
+		localStorage.setItem(id, JSON.stringify(d))
+		localStorage.setItem('id', id)
+	}
+}
+
+function cadastrarDespesa() {
+	let ano = document.getElementById('ano')
+	let mes = document.getElementById('mes')
+	let dia = document.getElementById('dia')
+	let tipo = document.getElementById('tipo')
+	let descricao = document.getElementById('descricao')
+	let valor = document.getElementById('valor')
+
+	let despesa = new Despesa(ano.value, mes.value, dia.value, 
+		tipo.value, descricao.value, valor.value)
+	let bd = new Bd()
+
+	if(despesa.validarDados()) {
+		bd.gravar(despesa)
+	} else {
+		alert('Preencha todos os dados')
+	}
+
+	
+}
