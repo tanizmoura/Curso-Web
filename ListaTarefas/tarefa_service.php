adfef1677f6d075525581c1d0ecdaeb5346c13f8
Tanise Moura <tanise.mourax3@hotmail.com>
2020-10-23 19:51:35 -0300

    Update tarefa_service.php

--- ListaTarefas/tarefa_service.php
+++ ListaTarefas/tarefa_service.php
@@ -28,6 +28,22 @@ class TarefaService {
 		return $lista;
 	}
 
+	public function recuperarTarefasPendentes() {
+		$query = '
+			select 
+				t.id, s.status, t.tarefa 
+			from 
+				tb_tarefas as t
+				left join tb_status as s on (t.id_status = s.id)
+			where
+				t.id_status = :id_status
+		';
+		$stmt = $this->conexao->prepare($query);
+		$stmt->bindValue(':id_status', $this->tarefa->__get('id_status'));
+		$stmt->execute();
+		return $stmt->fetchAll(PDO::FETCH_OBJ);
+	}
+
 	public function atualizar() {
 		$query = 'update tb_tarefas set tarefa = ? where id = ?';
 		$stmt = $this->conexao->prepare($query);
